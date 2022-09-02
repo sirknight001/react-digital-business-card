589c6cce3bc37295434289ba48a983310eac459a
sirknight001 <109674453+sirknight001@users.noreply.github.com>
2022-09-02 23:49:52 +0800

    Update Description.js

--- src/components/Description.js
+++ src/components/Description.js
@@ -9,6 +9,6 @@ export function Description() {
         <h2>About</h2>
         <p>I am a React frontend developer, currently residing in the Philippines. I have also an interest onto game development, which is my side hustle everytime I get bored. I always try my very best on things that I love doing, that includes being a developer.  </p>
         <h2>Interests</h2>
-        <p>Gamer. Musician. University scholar. Old soul. Pixelated stuffs. Weeboo. Manga reader. Travel geek. Photography. Cats.</p>
+        <p>Gamer. Musician. University scholar. Old soul. Pixelated stuff. Weeboo. Manga reader. Travel geek. Photography. Cats.</p>
     </div>
 }
