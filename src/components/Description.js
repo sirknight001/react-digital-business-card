803d71ce5fab0dd55a38752ff664c4d710282917
sirknight001 <109674453+sirknight001@users.noreply.github.com>
2022-09-02 23:59:24 +0800

    Update Description.js

--- src/components/Description.js
+++ src/components/Description.js
@@ -7,7 +7,7 @@ export function Description() {
         <button className="email"><img src={require('./email.png')} alt='email' id="email-image" />Email</button>
         <button className="linked-in"><img src={require('./linkedin.png')} alt='linkedin' id="li-image" />LinkedIn</button>
         <h2>About</h2>
-        <p>I am a React frontend developer, currently residing in the Philippines. I have also an interest onto game development, which is my side hustle everytime I get bored. I always try my very best on things that I love doing, that includes being a developer.  </p>
+        <p>I am a React frontend developer, currently residing in the Philippines. I have also an interest in game development, which is my side hustle everytime I get bored. I always try my very best on things that I love doing, that includes being a developer.  </p>
         <h2>Interests</h2>
         <p>Gamer. Musician. University scholar. Old soul. Pixelated stuff. Weeboo. Manga reader. Travel geek. Photography. Cats.</p>
     </div>
